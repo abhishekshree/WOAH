b500e1227fdb24f2831a939231405856c601dfc6
Abhishek Shree <abhi1234456@gmail.com>
2020-03-28 02:07:47 +0530

    Major Bugs Fixed

--- WOAHMUSIC/app.py
+++ WOAHMUSIC/app.py
@@ -1,13 +1,20 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
+# from werkzeug.security import generate_password_hash, check_password_hash
+import flask_login
+
 
 app = Flask(__name__)
-# signed_up = false a variable to restrict free movement
 db = SQLAlchemy(app)
 
+login_manager = flask_login.LoginManager()
+login_manager.init_app(app)
+
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///db.sqlite3"
+app.config['SECRET_KEY'] = "nahibataengetumkokuchbhibeta"
 db.init_app(app)
 
+#Models
 
 class Recommendations(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -23,27 +30,81 @@ class Final(db.Model):
     givenby = db.Column(db.String(100))
     
 
+users = {
+    "woah@gmail.com":"woahwoah",
+    "user@gmail.com":"bye"
+}
+
+#UserMixin
+class User(flask_login.UserMixin):
+    pass
+
+
+@login_manager.user_loader
+def user_loader(email):
+    if email not in users:
+        return
+
+    user = User()
+    user.id = email
+    return user
+
+
+@login_manager.request_loader
+def request_loader(request):
+    email = request.form.get('email')
+    if email not in users:
+        return
+
+    user = User()
+    user.id = email
+    user.is_authenticated = request.form['password'] == users[email]['password']
+
+    return user
+
+# Main Views
+global get
+get=False
+
+global loginMessage
+loginMessage=False
+
 
 @app.route('/')
 def main():
     Flist = Final.query.all()
-    return render_template("index.html", flist=Flist)
+    return render_template("index.html", flist=Flist, get=get)
 
 
 @app.route('/', methods= ['POST'])
 def getData():
+    
     sname = request.form.get('songname')
     singer = request.form.get('singer')
     user = request.form.get('uname')
     new_rec = Recommendations(Song_name=sname, Singer_name=singer, rec_name=user)
     db.session.add(new_rec)
     db.session.commit()
+    global get
+    get=True
 
     return redirect(url_for('main'))
 
+
 @app.route('/login')
 def login():
-    return render_template("login.html")
+    return render_template("login.html", loginMessage=loginMessage)
+
+
+@app.route('/logout')
+def logout():
+     flask_login.logout_user()
+     return redirect(url_for('main'))
+
+
+@login_manager.unauthorized_handler
+def unauthorized_handler():
+    return 'Unauthorized'
 
 
 @app.route('/login', methods=['POST'])
@@ -51,18 +112,27 @@ def loginCheck():
     emailcheck = request.form.get('email')
     password = request.form.get('pass')
 
-    if emailcheck == "woah@gmail.com" and password == "woahwoah":
+    if emailcheck in users and users.get(emailcheck) == password:
+        
+        user = User()
+        user.id = emailcheck
+        flask_login.login_user(user)
         return redirect(url_for('admin'))
     else:
+        global loginMessage
+        loginMessage = True
         return redirect(url_for('login'))
 
 
 @app.route('/admin')
+@flask_login.login_required
 def admin():
     recommendations = Recommendations.query.all()
     return render_template("admin.html", recommendations=recommendations)
 
+
 @app.route('/drop_song/<string:id>')
+@flask_login.login_required
 def drop_song(id):
     waste = Recommendations.query.filter_by(id=id).first()
     db.session.delete(waste)
@@ -82,10 +152,12 @@ def to_chart(id):
 
 
 @app.route('/add_music')
+@flask_login.login_required
 def add_music():
     return render_template("addMusic.html")
 
 @app.route('/add_music', methods=['POST'])
+@flask_login.login_required
 def add_music_admin():
     sname = request.form.get('sname')
     singer = request.form.get('singer')
@@ -96,12 +168,14 @@ def add_music_admin():
     return redirect(url_for('add_music'))
 
 @app.route('/view_chart')
+@flask_login.login_required
 def view_chart():
     final = Final.query.all()
     return render_template("viewChart.html", final=final)
 
 
 @app.route('/delete/<string:id>')
+@flask_login.login_required
 def delete(id):
     waste = Final.query.filter_by(id=id).first()
     db.session.delete(waste)
